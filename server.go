4d9884e6070fd3ddc37f312aeb9c000d04bebb35
Erik Sjöstedt <erik.sjoestedt@gmail.com>
2022-12-18 10:10:04 +0100

    Start implementing /validate
    Add simple input validation

--- server.go
+++ server.go
@@ -13,8 +13,18 @@ func main() {
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		fmt.Fprintf(w, "Hello world from GfG")
 	})
-	http.HandleFunc("/hi", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprintf(w, "Hi")
+	http.HandleFunc("/validate", func(w http.ResponseWriter, r *http.Request) {
+		iban, ok := r.URL.Query()["iban"]
+
+		if !ok || len(iban) == 0 {
+			http.Error(w, "IBAN is missing", 400)
+			return
+		}
+		if len(iban) != 1 {
+			http.Error(w, "Can only validate one IBAN per request", 400)
+			return
+		}
+		fmt.Println("IBAN: " + iban[0])
 	})
 
 	port := ":5000"
